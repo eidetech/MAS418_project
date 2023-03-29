e2aa9625fe2051fc160aaca3e80d91458e7c14b7
eidetech <marcuseide@me.com>
2023-03-30 00:48:00 +0200

    Removed .git folders, so that all packages are uploaded to this repo

---
+++ O3/ROS2/ros2-examples-ads/urdf_tutorial/build/urdf_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marcus/git/MAS418_project/O3/ROS2/ros2-examples-ads/urdf_tutorial
+# Build directory: /home/marcus/git/MAS418_project/O3/ROS2/ros2-examples-ads/urdf_tutorial/build/urdf_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
